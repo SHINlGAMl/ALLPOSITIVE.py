5133fd062c0b0acf2f3392d9938bec18210da1f7
SHINlGAMl <shawnjacob98@gmail.com>
2020-08-04 13:28:20 +0400

    ALL POSITIVE
    THIS CODE FILTERS OUT THE NEGATIVE INTEGERS FORM THE LIST

---
+++ allpositive.py
@@ -0,0 +1,14 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+for x in list1:
+    if x < 0:
+        continue
+    else:
+        print(x)
+
+for x in list2:
+    if x < 0:
+        continue
+    else:
+        print(x)
